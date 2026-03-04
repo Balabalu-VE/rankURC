a77d77966e2b187b7108b90d3f322236d6a1e4ab
Balabalu-VE <nicolasdittmarg1@gmail.com>
2026-03-04 02:20:31 -0600

    updated main.py

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ engine = create_engine(DATABASE_URL)
 @app.get("/")
 def home():
         return {"message": "Ranking app running"}
+
+@app.get("/init")
+def init():
+        with engine.connect() as conn:
+                    conn.execute(text("""
+                                CREATE TABLE IF NOT EXISTS items (
+                                                id SERIAL PRIMARY KEY,
+                                                                name TEXT,
+                                                                                rating FLOAT DEFAULT 1200
+                                                                                            )
+                                                                                                    """))
+                            conn.commit()
+                                return {"status": "table created"}
